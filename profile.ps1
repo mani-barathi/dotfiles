9f923c704d8ce7fe53938a7a4047dde690f812c0
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2021-04-02 11:40:41 +0530

    updated profile.ps1 with shortcuts & touch command

--- profile.ps1
+++ profile.ps1
@@ -1,15 +1,41 @@
-function IsAdmin{  
-    [OutputType([bool])]
-    param()
-    process {
-        [Security.Principal.WindowsPrincipal]$user = [Security.Principal.WindowsIdentity]::GetCurrent();
-        return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
-    }
+# Author      : Manibarathi
+# GitHub	  : https://github.com/mani-barathi/dotfiles
+# Description : profile file for my Customized Powershell Prompt
+#
+# This file should be stored in $PROFILE.CurrentUserAllHosts, if it doesn't exists already create using below command
+# command:- New-Item $PROFILE.CurrentUserAllHosts -ItemType File -Force
+# To enable unsigned script execution of local scripts run the below command as Admin
+# command:- Set-ExecutionPolicy -ExecutionPolicy RemoteSigned 
+
+
+# Find out if the current user has admin rights
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# shortcuts for switching directories (Modify if Required)
+function cdd  { cd C:\Users\manib\Desktop }
+function cdr  { cd C:\Users\manib\Desktop\random }
+function cddoc  { cd C:\Users\manib\Documents}
+function cddownloads { cd C:\Users\manib\Downloads}
+function cdp  { cd H:\Projects}
+
+# touch command to create any file from the powershell
+function touch{
+    $files = $args
+	Foreach($file in $files ){
+		if(Test-Path $file){
+			Write-Output "ERROR: $file already exists"
+		}
+		else{
+			New-Item -ItemType File -Name ($file)
+		}
+	}
 }
 
+
 function Prompt {
-	
-	if((IsAdmin)){
+	if($IsAdmin){
 		write-host "Admin" -NoNewline -ForegroundColor Cyan -BackgroundColor DarkRed 
 	}
 	else{
